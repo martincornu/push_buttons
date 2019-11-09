60b8435d7231e265ad4bd284b01f2a5f3fecd592
martin <martin@mart.home>
2019-11-09 22:56:38 +0100

    deactivate an ouput if suite of press is correct

--- push_buttons/push_buttons.ino
+++ push_buttons/push_buttons.ino
@@ -1,18 +1,27 @@
 /*
   State change detection (edge detection).
-
+  Puzzle game
   created  9 Nov 2019
   modified 9 Nov 2019
   by Martin CORNU
+
+  Suite de boutons à appuyer dans le bon ordre (boutons 1 à 5)
+  -> 1;2;3;4;5
+  
 */
 
-/***************************** VARIABLES ***************************************/
-const uint8_t  buttonPinA = 2;       // the pin that the pushbutton is attached to
-const uint8_t  ledPin = LED_BUILTIN; // the pin that the LED is attached to
 
-uint8_t buttonPushCounterA = 0;   // counter for the number of button presses
-uint8_t buttonStateA = 0;         // current state of the button
-uint8_t lastButtonStateA = 0;     // previous state of the button
+/***************************** VARIABLES ***************************************/
+const uint8_t numberOfButtons = 5;                                  // Nombre de boutons
+const uint8_t  arr_buttonPin[numberOfButtons] = { 2, 3, 4, 5, 6 };  // Numero des pins auxquelles sont branches les boutons
+const uint8_t arr_suite[numberOfButtons] = {2, 3, 4, 5, 6};         // Suite a trouver (numero des pins) 
+
+uint8_t suiteIndex = 0;                                       // index in the suite
+uint8_t arr_buttonPushCounter[numberOfButtons] = {0};         // counter for the number of button presses
+uint8_t arr_buttonState[numberOfButtons] = {0};               // current state of the button
+uint8_t arr_lastButtonState[numberOfButtons] = {0};           // previous state of the button
+
+const uint8_t  ledPin = 12; // the pin that the LED is attached to
 
 
 /***************************** FUNCTIONS ***************************************/
@@ -29,6 +38,21 @@ void checkButton(uint8_t buttonPin, uint8_t * buttonState, uint8_t * lastButtonS
       Serial.println("\non");
       Serial.print("number of button pushes: ");
       Serial.println(*buttonPushCounter);
+
+      if (buttonPin == arr_suite[suiteIndex]) {
+        suiteIndex++;
+      } else {
+        suiteIndex = 0;
+      }
+
+      if (suiteIndex == numberOfButtons) {
+        digitalWrite(ledPin, HIGH);
+        suiteIndex = 0;
+      } else {
+        digitalWrite(ledPin, LOW);
+      }
+
+      
     } else {
       // if the current state is LOW then the button went from on to off:
       Serial.println("off");
@@ -51,23 +75,25 @@ void checkPushes(uint8_t buttonPushCounter) {
 
 /***************************** MAIN ***************************************/
 void setup() {
-  // initialize the button pin as a input:
-  pinMode(buttonPinA, INPUT);
+
+  for (int i=0; i<numberOfButtons; ++i) {
+    pinMode(arr_buttonPin[i], INPUT);
+  }
+  
   // initialize the LED as an output:
   pinMode(ledPin, OUTPUT);
   digitalWrite(ledPin, LOW);
+  
   // initialize serial communication:
   Serial.begin(9600);
 }
 
 
-void loop() {
+void loop() { 
 
-  checkButton(buttonPinA, &buttonStateA, &lastButtonStateA, &buttonPushCounterA);
+  for (int i=0; i<numberOfButtons; ++i) {
+    checkButton(arr_buttonPin[i], &arr_buttonState[i], &arr_lastButtonState[i], &arr_buttonPushCounter[i]);
+    delay(20);
+  }
   
-  // Delay a little bit to avoid bouncing
-    delay(50);
-
-  checkPushes(buttonPushCounterA);
-
 }
